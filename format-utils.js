fef74851c7b72161e0f8fb8727f70eb633ec29a0
Pradeepa <pradeepa@gmail.com>
2026-07-14 14:55:26 +0530

    Initial package

---
+++ format-utils.js
@@ -0,0 +1,12 @@
+function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function reverse(text) {
+    return text.split("").reverse().join("");
+}
+
+module.exports = {
+    capitalize,
+    reverse
+};
