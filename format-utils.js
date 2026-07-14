20aab8ccffe0c132edb97ee3b5672e483209bf28
pradeepathirumurthi70-star <pradeepathirumurthi70@gmail.com>
2026-07-14 20:33:44 +0530

    Remove reverse function from format-utils.js
    Removed the reverse function from format-utils.js.

--- format-utils.js
+++ format-utils.js
@@ -5,11 +5,9 @@ function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-function reverse(text) {
-    return text.split("").reverse().join("");
-}
+
 
 module.exports = {
     capitalize,
-    reverse
+   
 };
