a8c9df05031e5768fe9283195f1d0bc53abde6cf
pradeepathirumurthi70-star <pradeepathirumurthi70@gmail.com>
2026-07-14 20:39:22 +0530

    Update format-utils.js

--- format-utils.js
+++ format-utils.js
@@ -1,13 +1,16 @@
-function capitalize(text) {
-    return text.toUpperCase();
+// format-utils.js
+
+/**
+ * Capitalizes the first letter of a given string.
+ * @param {string} str - The input string.
+ * @returns {string} The capitalized string.
+ */
+function capitalize(str) {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
-{
-    return text.charAt(0).toUpperCase() + text.slice(1);
-}
 
-
-
+// Export the function so other projects can import it.
 module.exports = {
-    capitalize,
-   
+    capitalize
 };
